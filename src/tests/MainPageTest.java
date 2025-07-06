5bed4656cd0312cf63b7ab4698b2e7eddca9c286
Yudessa <Darksoul452@mail.ru>
2025-07-06 18:50:19 +0300

    урок 4.7. поиск в bing, переход по первой ссылке и проверка на соответствие.

--- src/tests/MainPageTest.java
+++ src/tests/MainPageTest.java
@@ -8,10 +8,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class MainPageTest {
 
@@ -20,19 +25,15 @@ public class MainPageTest {
     @BeforeEach
     public void setUp() {
         FirefoxOptions options = new FirefoxOptions();
-
         driver = new FirefoxDriver(options);
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.bing.com/");
     }
-
 
     @AfterEach
     public void tearDown() {
         driver.quit();
     }
-
 
     @Test
     public void search() {
@@ -41,7 +42,28 @@ public class MainPageTest {
         searchField.sendKeys(input);
         searchField.submit();
 
-        WebElement searchPageField = driver.findElement(By.cssSelector("#sb_form_q"));
-        assertEquals(input, searchPageField.getAttribute("value"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//a[text()='Selenium' and @target='_blank']")
+        ));
+
+        List<WebElement> results = driver.findElements(By.xpath("//a[text()='Selenium' and @target='_blank']"));
+
+        String originalTab = driver.getWindowHandle();
+
+        clickElement(results, 0);
+
+        wait.until(ExpectedConditions.numberOfWindowsToBe(2));
+
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+
+        wait.until(ExpectedConditions.urlContains("selenium.dev"));
+        assertTrue(Objects.requireNonNull(driver.getCurrentUrl()).contains("selenium.dev"), "Неверный URL");
+    }
+
+    public void clickElement(List<WebElement> results, int num) {
+        results.get(num).click();
+        System.out.println(results);
     }
 }
